cc29edcc113ebc0a2b0b1f9dc7bd9ffd7f3f63ee
josh171220 <164979878+josh171220@users.noreply.github.com>
2024-03-28 15:57:50 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Josh Registry',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://josh171220.github.io/kasm-registry/1.0/',
+    listUrl: 'https://josh171220.github.io/kasm-registry/',
     contactUrl: 'https://github.com/josh171220/kasm-registry/issues',
   },
   reactStrictMode: true,
